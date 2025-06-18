c386d89b9b43c2999dc1d09a143457360ca5cb62
francesca.castangia <castangiaf2@gmail.com>
2025-06-18 10:21:26 +0200

    esercizio Props

---
+++ Props/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Age } from './Age'
+import { Welcome } from './Welcome'
+
+
+function App() {
+ 
+  return (
+    <div>
+      <Welcome name="Mario" />
+      < Age age={25} />
+    </div>
+  )
+}
+
+export default App
