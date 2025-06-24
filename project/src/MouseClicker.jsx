921a4ea1a2b15f4e4933f6ef9252818db346f315
francesca.castangia <castangiaf2@gmail.com>
2025-06-24 10:10:53 +0200

    fixato

---
+++ project/src/MouseClicker.jsx
@@ -0,0 +1,22 @@
+export function MuoseClicker (){
+    function handleButtonClick(event){
+        console.log(event.currentTarget.name)
+    }
+
+    function handleImageClick(event){
+        event.stopPropagation();
+        console.log(event.currentTarget.name);
+    }
+
+    return(
+        <div>
+            <button name="one" onClick={handleButtonClick}>Click me</button>
+            <button name="two" onClick={handleButtonClick}>
+                <img width={24} height={24} onClick={handleImageClick} />
+                Click me
+            </button>
+        </div>
+    )
+}
+
+export default MuoseClicker
