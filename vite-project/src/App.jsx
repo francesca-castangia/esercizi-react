29db95f74ca05b5d4acb111a6513da6648dc35d6
francesca.castangia <castangiaf2@gmail.com>
2025-07-20 16:07:44 +0200

    corretto swr

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
-import { Form } from "./components/Form";
+
+import { Route, Routes } from "react-router-dom";
 import { GithubUser } from "./components/GithubUser";
-import { CurrentLocation } from "./components/CurrentLocation";
+
 
 
  function App() {
@@ -11,19 +9,10 @@ import { CurrentLocation } from "./components/CurrentLocation";
  
   return (
     <div >
-      <nav>
-        <ul>
-          <li><Link to={'/'}>Counter</Link></li>
-          <li><Link to={'/form'}>Form</Link></li>
-          <li><Link to={'/user'}>User</Link></li>
-          <li><Link to={'/clocation'}>Current Location</Link></li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" element={<Counter/>}/>
-        <Route path="/form" element={<Form/>}/>
-        <Route path="/user" element={<GithubUser />}/>
-        <Route path="/clocation" element={<CurrentLocation/>}/>
+  
+        <Route path="/" element={<GithubUser />}/>
+        
       </Routes>
       
     </div>
